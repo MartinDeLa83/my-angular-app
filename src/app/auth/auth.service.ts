530385938d0138d3960ae8d78c89ed7f3cb6b6d0
Martín de la Cruz Delgado <martindelacruzdelgado83@gmail.com>
2019-07-13 12:49:52 +0200

    Update auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,8 +15,8 @@ const httpOptions = {
 })
 export class AuthService {
 
-  private loginUrl = "http://13.92.212.69:8080/api/auth/signin";
-  private signupUrl = "http://13.92.212.69:8080/api/auth/signup";
+  private loginUrl = "http://104.45.157.90:8080/api/auth/signin";
+  private signupUrl = "http://104.45.157.90:8080/api/auth/signup";
 
   constructor(private http: HttpClient) {
   }
